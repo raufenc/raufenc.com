14f1f50f729da0b92a36b322e58065c7169a5e2a
Rauf <raufenc@gmail.com>
2026-03-27 00:39:22 +0300

    Kayı Atlası: Osmanlı padişahları interaktif tarih haritası eklendi

---
+++ arapca/unite2/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'tasawwuq-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.js',
+  'https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400&family=Noto+Sans+Arabic:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
